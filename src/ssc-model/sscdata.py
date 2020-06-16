148cade3c182d9b06a054b80564bec46bcce37c7
Ava Spataru <ms19avs@tr01.tcs.cs.ox.ac.uk>
2020-06-16 19:28:15 +0100

    added explanations on SSC code

--- src/ssc-model/sscdata.py
+++ src/ssc-model/sscdata.py
@@ -7,42 +7,54 @@ from tooldata import *
 #needed order of nucleotides as defined by the SSC model (A-1, C-2, G-3, T-4)
 nucleotides = ['A','C','G','T']
 
+
+#############################################################################
+## Adaptation of C++ code from SSC source code
+## See: https://sourceforge.net/projects/spacerscoringcrispr/
+############################################################################
+
 class MatrixModel:
 
     matrix = [[]]
 
+    #Loading a matrix of coefficients which will be used to score the sgRNAs
     def loadMatrix(self):
         matrixFile = open('./ssc-model/human_CRISPRi_20bp.matrix','r')
         lines = matrixFile.readlines()
-        lines.pop(0) #intercept (at 0)
-        lines.pop(0) #header
+        lines.pop(0) #skip intercept (at 0)
+        lines.pop(0) #skip header (A,C,G,T)
         matrixFile.close()
 
         matrix = []
         row = 1
-        for line in lines:
+        for line in lines: #get the coefficients for position row
             coefs = line.strip().split('\t')
             coefs = [float(coef) for coef in coefs]
-            matrix.append(coefs)
+            matrix.append(coefs) #store this row of coefficients
             row = row + 1
 
         self.matrix = matrix
 
+    # For a set of data, to predict a datapoint simply add up the coefficients
+    # Expects data to have the features (i.e. if at position 1 there is A, C, G, T; if at position 2.... and etc.)
     def predict(self, data):
-        nucleotides = ['A','C','G','T']
         results = []
         for d in data:
-            score = 0
+            score = 0 #intercept is at 0
             #one data point is a list of features (0-80)
-            for f in range(0,20):
-                for n in range(0,4):
+            for f in range(0,20): #position f
+                for n in range(0,4): #the nth nucleotide
                     #one feature is if the nucleotide is there
                     if(d[f*4 + n] == 1): #this nucleotide is present in position f
-                        score = score + self.matrix[f][n]
+                        score = score + self.matrix[f][n] #add the coefficient
             results.append(score)
 
         return np.array(results)
 
+#############################################################################
+## This class defines the necessary code for the SSC tool to be called in
+## by the shapvals.py script.
+############################################################################
 class SSCData(ToolData):
 
     #the training set is the Xu dataet
