6df39220b6fbe95891ab880d76724cedb734e196
Ava Spataru <ms19avs@tr02.tcs.cs.ox.ac.uk>
2020-06-16 16:02:17 +0100

    Command line arguments for tool and dataset name

--- src/shapleyvals.py
+++ src/shapleyvals.py
@@ -1,6 +1,7 @@
 import shap
 import pandas as pd
 import pickle
+import argparse
 
 from tuscandata import *
 from sgRNAScorer2data import *
@@ -12,24 +13,22 @@ def getDataset(datasetName, toolName):
 
     sequences = []
 
-    if datasetName == 'Xu':
+    if datasetName == 'xu':
         #WHERE THE DATASET IS
         INPUT_FILE_NAME = "/users/ms19avs/Documents/proj/datasets/Xu-2015_Is-Efficient.csv"
+        GUIDE_INDEX = 4 #column of the guide in the file
 
         # Used for extracting the guide from the CSV format
         if (toolName in ['tuscan-regression','tuscan-classification']): #Tuscan has a special extraction of data since it uses context around the guide
-            GUIDE_INDEX = 4
             GUIDE_START_POS = 10 - 4
             GUIDE_LEN = 23 + 4 + 3 # TUSCAN considers nucleotides 4 upstream, 3 upstream from guide (?=([ACTG]{25}GG[ACTG]{3}))
         elif toolName == 'wu-crispr': #it needs 3 more nucleotides after the guide
-            GUIDE_INDEX = 4
             GUIDE_START_POS = 10
             GUIDE_LEN = 23 + 3
         elif toolName == 'ssc': #it doesn't take the PAM
-            GUIDE_INDEX = 4
             GUIDE_START_POS = 10
             GUIDE_LEN = 20
-        else:
+        else: #default for all the other tools
             GUIDE_INDEX = 4
             GUIDE_START_POS = 10
             GUIDE_LEN = 23
@@ -40,20 +39,21 @@ def getDataset(datasetName, toolName):
             for line in [x.split(',') for x in fRead.read().split('\n')[1:-1]]:
                 guideSeq = line[GUIDE_INDEX][GUIDE_START_POS:(GUIDE_START_POS + GUIDE_LEN)]
                 sequences.append(guideSeq)
-
         return sequences
-    elif datasetName =='Doench':
+
+    elif datasetName =='doench':
         print("Loading Doench not yet here")
         quit()
     else:
         print("Dataset not supported")
         quit()
 
+#returns the object corresponding to the requested tool
 def getToolObject(toolName):
     if toolName in ['tuscan-regression','tuscan-classification']:
         tool = TuscanData()
         tool.setRegressionFlag(toolName=='tuscan-regression')
-    elif toolName == 'sgRNAScorer2':
+    elif toolName == 'sgrnascorer2':
         tool = SgRNAScorer2Data()
     elif toolName == 'wu-crispr':
         tool = WuCrisprData()
@@ -64,54 +64,60 @@ def getToolObject(toolName):
         quit()
     return tool
 
+#runs the Shapley Value analysis on the requested dataset and tool
 def main(toolName,datasetName):
 
     tool = getToolObject(toolName)
 
-    tool.loadModel().predict([tool.getFeatures("AAGCCAAAGAGAAAGGTACC")])
-
-    train_df = tool.loadTrainingSet()
-
-    #load the dataset
+    #load the dataset [used as daa in shapley plot]
     sequences = getDataset(datasetName, toolName)
     print("Loaded the dataset " + datasetName + " of " + str(len(sequences)) + " data points.")
 
-    #calculate the features for the dataset
+    #Calculate the features for the dataset
     print("Calculating features for the dataset. [Might take a while for wu-crispr]")
     feature_set = []
     cnt = 1
     for seq in sequences:
         features = tool.getFeatures(seq)
         feature_set.append(features)
-        if toolName == 'wu-crispr' and cnt % 100 == 0: #inform on progress
+        if toolName == 'wu-crispr' and cnt % 100 == 0: #inform on progress [wu-crispr takes a while]
             print("Calculated features for " + str(cnt))
         cnt = cnt + 1
     print("Calculated the features for this dataset.")
 
-    #get feature names
+    #Get feature names [for printing in the shapley plot]
     feature_names = tool.loadFeatureNames()
     print("Loaded the names of all " + str(len(feature_names)) + " features.")
 
-    #put together features with names in one dataframe
+    #Put together features with names in one dataframe
     dataset_df = pd.DataFrame(np.array(feature_set),columns=feature_names)
 
     #training set (must be loaded before model)
     train_df = tool.loadTrainingSet()
     print("Loaded the training set used for tool " + toolName + ", size: " + str(train_df.shape)+ ".")
 
-    #load model
+    #load model of the tool
     model = tool.loadModel()
     print("Loaded the model for tool " + toolName + ".")
 
     #summarize training set and subsample test set
     summary_train_df = shap.kmeans(train_df,2)
-    dataset_sub_df = dataset_df # dataset_df.sample(400)
+    dataset_sub_df = dataset_df # optional to speed up things can use dataset_df.sample(400)
 
     #compute and plot shapley values
     svm_shap_values = shap.KernelExplainer(model.predict,summary_train_df)
     shap.summary_plot(svm_shap_values.shap_values(dataset_sub_df), dataset_sub_df)
 
 
-toolName = "ssc"
-datasetName = "Xu"
+
+
+#get the tool name and dataset name to start analysis
+
+parser = argparse.ArgumentParser(description="specify the tool name and dataset name.")
+parser.add_argument('--tool', help='name of the tool to be analysed', required=True)
+parser.add_argument('--data', help='name of the dataset to run the tool on', required=True)
+args = parser.parse_args()
+
+toolName = args.tool.lower()
+datasetName = args.data.lower()
 main(toolName,datasetName)
