a5a726c411a254c588d4046428f4ae2d411fddca
Ava Spataru <ms19avs@tr02.tcs.cs.ox.ac.uk>
2020-06-19 14:55:37 +0100

    chop-chop standard feature names

---
+++ src/chop-chop-model/chopchopdata.py
@@ -0,0 +1,20 @@
+
+class CoefficientModel:
+
+
+
+class ChopChopData(ToolData):
+
+    @staticmethod
+    def loadTrainingSet(self):
+        pass
+
+    @staticmethod
+    def loadFeatureNames(self):
+        pass
+
+    def loadModel(self):
+        pass
+
+    def getFeatures(self,seq):
+        pass
