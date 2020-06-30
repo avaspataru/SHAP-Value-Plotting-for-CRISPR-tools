d92db9c7faedb7f00859be313301b20d488fc3ec
Ava Spataru <ms19avs@tr02.tcs.cs.ox.ac.uk>
2020-06-30 14:02:27 +0100

    parsed positional features

--- src/analyse.py
+++ src/analyse.py
@@ -2,39 +2,24 @@ import pickle
 import argparse
 import shap
 import sys
+import re
+import matplotlib.pyplot as plt
+import numpy as np
 
-def getFeatureNames(toolName):
-    if toolName in ['tuscan-regression','tuscan-classification']:
-        sys.path.insert(0,'../src/tuscan-model')
-        from tuscandata import TuscanData
-        tool = TuscanData()
-        tool.setRegressionFlag(toolName=='tuscan-regression') #if regression
-    elif toolName == 'sgrnascorer2':
-        sys.path.insert(0, '../src/sgRNAScorer2-model')
-        from sgRNAScorer2data import SgRNAScorer2Data
-        tool = SgRNAScorer2Data()
-    elif toolName == 'wu-crispr':
-        sys.path.insert(0,'../src/wu-crispr-model')
-        from wucrisprdata import WuCrisprData
-        tool = WuCrisprData()
-    elif toolName == 'ssc':
-        sys.path.insert(0, '../src/ssc-model')
-        from sscdata import SSCData
-        tool = SSCData()
-    elif toolName in ['chop-chop-xu', 'chop-chop-doench', 'chop-chop-moreno']:
-        #ASSUME Xu scoring method = TO DO CHANGE
-        sys.path.insert(0, '../src/chop-chop-model')
-        from chopchopdata import ChopChopData
-        tool = ChopChopData()
-        tool.setScoring(toolName)
-    else:
-        print("Tool Name not valid")
-        quit()
+from utils import getToolObject
 
+nucleotides = ['A','C','G','T']
 
+#gets the feature names for the tool which was used to produce the pickle file
+def getFeatureNames(fileName):
+
+    #get the tool corresponding to the file name
+    toolName = fileName.split('-')[1]
+    tool = getToolObject(toolName) #from utils.py
+
+    #get the actual feature names from the tool object
     featureNames = tool.loadFeatureNames()
-    print(featureNames)
-    quit()
+    return featureNames
 
 #get the pickle file from arguments
 parser = argparse.ArgumentParser(description="specify the file to unpicke for the SHAP values")
@@ -46,9 +31,55 @@ fileName = args.file
 #load the SHAP values and the data
 p = open('../results/'+fileName,"rb")
 print("Loading data...")
-shap_values = pickle.load(p)
+shapValues = pickle.load(p)
 dataset = pickle.load(p)
 p.close()
 
-#feature Names for the tool the pickle was created with
-feature_names = getFeatureNames( fileName.split('-')[1] )
+#feature names for the tool the pickle was created with
+featureNames = getFeatureNames( fileName )
+
+#calculate the average shap value across all datapoints for a feature
+print("Calculating averages...")
+avgShapVals = [0] * len(featureNames) #initialise
+
+#shapValues = dataset x features
+for d in range(0,len(dataset)):
+    for f in range(0,len(featureNames)):
+        avgShapVals[f] = avgShapVals[f] + shapValues[d][f] #add the value from this datapoint
+
+#average out the sum
+avgShapVals = [ v / len(dataset) for v in avgShapVals]
+
+
+#sort the features by the absolute value of the avg Shap value
+print("Getting positional values...")
+featureImp = sorted( zip(avgShapVals,featureNames) , key = lambda t: abs(t[0])) #zip with the feature names and sort
+#featureImp = featureImp[-20:] # take the top 20 highest values <=> most important 20 features
+featureImp = list(reversed(featureImp)) #order from most important to least important
+
+#create dictionary from list of feature importances
+#where averages[pos][nucleotide] = the shapley value of the feature pos:nucleotide
+averages = np.zeros((20,4))
+matches = 0 #number of positional features found
+for f in featureImp:
+    featureValue = f[0] #the shapley value of this feature
+    featureName = f[1] #the name of this feature
+
+    #matching the feature name to something containing position-nucleotide [only one nucleotide!]
+    #(position) (maybe :) (nucleotide) (anything but another nucleotide)
+    m = re.match(r"(?P<pos>((\d)(\d)*))(:)?(?P<nuc>[ACGT])([^ACGT](.*))?$", featureName)
+
+    if m == None: #maybe the position and nucleotide are in reverse order
+        print("rev")
+        #(something + not a nucleotide or start) (nucleotide) (maybe :) (pos) (not digit + something or end)
+        m = re.match(r"^((.*)[^ACGT])?(?P<nuc>[ACGT])(:)?(?P<pos>((\d)(\d)*))([^0-19](.*))?$", featureName)
+
+    if m == None: #not a positional feature
+        continue
+    matches = matches + 1
+    pos = int( (m.group('pos')) )
+    nuc = nucleotides.index(m.group('nuc')) # A:0, C:1, G:2, T:3
+
+    averages[pos][nuc] = featureValue
+
+assert matches == 80 #can only have 80 positional features
